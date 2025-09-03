11b01da41b47cfe39cc6e8034e40b3ba6938afca
adwik314 <adwik314@gmail.com>
2025-09-03 11:40:01 +0530

    Create Q1.c
    it calculates sum of two numbers

---
+++ Q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float a, b, Sum ;
+//TAKING 1ST INPUT FROM THE USER
+  printf("enter 1st Number\n");
+  scanf("%f", &a);
+   //TAKING 2ND INPUT FROM THE USER
+printf("enter 2nd Number\n");
+scanf("%f" , &b);
+//calculating sum
+printf("Sum = %f" , a + b);
+return 0;
+
+}
