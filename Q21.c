57f085313fa6fac5f513d73927f07f59bf598700
adwik314 <adwik314@gmail.com>
2025-10-17 21:31:56 +0530

    Create Q21.c
    Program to display the month name and number of days using switch-case for a given month number.

---
+++ Q21.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int n;
+    //taking input from the user
+    printf("Enter the Month Number (1-2): \n");
+    scanf("%d", &n);
+
+    if(n>=1 && n<=12){
+        switch (n){
+            case 1: printf("January = 31 days\n");break;
+            case 2: printf("February = 28 days\n");break;
+            case 3: printf("March = 31 days\n");break;
+            case 4: printf("April = 30 days\n");break;
+            case 5: printf("May = 31 days\n");break;
+            case 6: printf("June = 30 days\n");break;
+            case 7: printf("July = 31 days\n");break;
+            case 8: printf("August = 31 days\n");break;
+            case 9: printf("September = 30 days\n");break;
+            case 10: printf("October = 31 days\n");break;
+            case 11: printf("November = 30 days\n");break;
+            case 12: printf("December = 31 days\n");break;
+
+        }
+    }else {
+        printf("Invalid Input Please enter the month number from (1-2)\n");
+    }
+    return 0;
+}
