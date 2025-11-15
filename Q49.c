a6a0d828483fc188568acfb59edeaa683f1ded55
adwik314 <adwik314@gmail.com>
2025-11-15 14:42:50 +0530

    Create Q49.c
    Program to print the following pattern:
    5
    45
    345
    2345
    12345

---
+++ Q49.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int n = 5;
+    for(int i=0;i<n;i++){
+        int temp = n - i;
+        for(int j=0;j<=i;j++){
+            printf("%d", temp);
+            temp++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+
+
