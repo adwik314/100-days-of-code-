eba0d4dc1930bf3208e8b3aaab147135208d3326
adwik314 <adwik314@gmail.com>
2025-11-18 18:02:48 +0530

    Create Q52.c
    program to print the following pattern:
    
    *
    
    *
    *
    *
    
    *
    *
    *
    *
    *
    
    *
    *
    *
    
    *

---
+++ Q52.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n = 5;
+    int nst = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=nst;j++){
+            printf("*\n");
+        }
+        printf("\n");
+        if(i<n/2+1) nst+=2;
+        else nst-=2;
+    }
+    return 0;
+}
